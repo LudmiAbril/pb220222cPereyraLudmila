f4ae2e5786f88be7f1a2375cab3fda9178065960
Ludmila <monntedeoca@hotmail.com>
2022-11-18 11:55:49 -0300

    primer commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Ludmila
+ *
+ */
+module pruebaGit {
+}
---
+++ src/pruebaGit/Prueba.java
@@ -0,0 +1,10 @@
+package pruebaGit;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
