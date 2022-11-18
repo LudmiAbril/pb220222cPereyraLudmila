79eda23c759ee338dbf37319226027441938db67
LudmiAbril <ludmila.pereyra543@gmail.com>
2022-11-18 13:29:29 -0300

    mas comentarios

--- src/pruebaGit/Prueba.java
+++ src/pruebaGit/Prueba.java
@@ -5,6 +5,7 @@ public class Prueba {
 	public static void main(String[] args) {
 		System.out.println("holass");
 		// esto es un prueba de git para el parcial
+		// aca hice otro comentario
 
 	}
 
